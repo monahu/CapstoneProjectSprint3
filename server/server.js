2ffe3ea5c675e84db03f4bd3049814412b188942
Aung <aungkokhant2213@gmail.com>
2025-05-26 23:03:14 -0400

    Change server port number to 3500

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
 // MongoDB Connection
 mongoose

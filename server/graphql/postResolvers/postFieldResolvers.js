89b447c58a047c1b1bbe6ca673bdd962c8f339c2
Aung <aungkokhant2213@gmail.com>
2025-06-19 09:56:46 -0400

    define constants

---
+++ server/graphql/postResolvers/postFieldResolvers.js
@@ -0,0 +1,10 @@
+const User = require("../../models/User");
+const Rating = require("../../models/Rating");
+const WantToGo = require("../../models/WantToGo");
+const Like = require("../../models/Likes");
+const PostsTags = require("../../models/PostsTags");
+const Tag = require("../../models/Tags");
+const {
+  DEFAULT_USER_DISPLAY_NAME,
+  DEFAULT_USER_PHOTO_URL,
+} = require("../../utils/constant");

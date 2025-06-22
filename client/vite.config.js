b005a4209b2dc9fc2343d92c529179b2b30e0bf3
monahu <monahu2022@gmail.com>
2025-06-22 16:52:26 -0400

    add vite-plugin-compression

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import viteCompression from "vite-plugin-compression"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteCompression()],
   test: {
     environment: "jsdom",
     setupFiles: ["./src/setupTests.js"],

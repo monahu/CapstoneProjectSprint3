1840d5bb2eb2e32d577b079c90f41742c7a489dc
João Couto <joaocouto2000@gmail.com>
2025-07-06 10:46:26 -0400

    Added speech to text functionality to the login form

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -1,28 +1,28 @@
-import { Formik, Form } from 'formik'
-import { getValidationSchema } from '../../utils/validationSchema'
+import { Formik, Form } from "formik";
+import { getValidationSchema } from "../../utils/validationSchema";
 import {
   SignUpFields,
-  EmailField,
-  PasswordField,
   ConfirmPasswordField,
-} from './FormFields'
-import { UI_TEXT } from '../../utils/constants/ui'
-import { FORM_CONFIG } from '../../utils/constants/form'
+} from "./FormFields";
+import { UI_TEXT } from "../../utils/constants/ui";
+import { FORM_CONFIG } from "../../utils/constants/form";
+import FieldWithMic from "../Speech/FieldWithMic";
+import SpeechButton from "../Speech/SpeechButton";
 
 const LoginForm = ({ isSignInForm, onSubmit, isLoading }) => {
   const handleSubmit = async (values, { setSubmitting }) => {
     try {
       if (onSubmit) {
-        await onSubmit(values)
+        await onSubmit(values);
       }
     } finally {
-      setSubmitting(false)
+      setSubmitting(false);
     }
-  }
+  };
 
   return (
     <Formik
-      key={isSignInForm ? 'signin' : 'signup'}
+      key={isSignInForm ? "signin" : "signup"}
       initialValues={
         isSignInForm
           ? FORM_CONFIG.initialValues.signIn
@@ -31,20 +31,31 @@ const LoginForm = ({ isSignInForm, onSubmit, isLoading }) => {
       validationSchema={getValidationSchema(isSignInForm)}
       onSubmit={handleSubmit}
     >
-      {() => (
-        <Form className='space-y-6'>
+      {({ setFieldValue }) => (
+        <Form className="space-y-6">
           {!isSignInForm && <SignUpFields />}
 
-          <EmailField />
-          <PasswordField />
+          <FieldWithMic name="email" type="email">
+            <SpeechButton
+              fieldName="email"
+              setFieldValue={setFieldValue}
+            />
+          </FieldWithMic>
+
+          <FieldWithMic name="password" type="password">
+            <SpeechButton
+              fieldName="password"
+              setFieldValue={setFieldValue}
+            />
+          </FieldWithMic>
 
           {!isSignInForm && <ConfirmPasswordField />}
 
-          <div className='flex items-center justify-between'>
-            <div className='text-sm/6'>
+          <div className="flex items-center justify-between">
+            <div className="text-sm/6">
               <a
-                href='#'
-                className='font-semibold text-indigo-600 hover:text-indigo-500'
+                href="#"
+                className="font-semibold text-indigo-600 hover:text-indigo-500"
               >
                 {UI_TEXT.login.forgotPassword}
               </a>
@@ -53,26 +64,21 @@ const LoginForm = ({ isSignInForm, onSubmit, isLoading }) => {
 
           <div>
             <button
-              type='submit'
+              type="submit"
               disabled={isLoading}
-              className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50'
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50"
             >
               {isLoading
                 ? UI_TEXT.login.processing
                 : isSignInForm
-                  ? UI_TEXT.buttons.logIn
-                  : UI_TEXT.buttons.signUp}
+                ? UI_TEXT.buttons.logIn
+                : UI_TEXT.buttons.signUp}
             </button>
           </div>
         </Form>
       )}
     </Formik>
-  )
-}
+  );
+};
 
-/* LoginForm.propTypes = {
-  isSignInForm: PropTypes.bool.isRequired,
-  onSubmit: PropTypes.func,
-}; */
-
-export default LoginForm
+export default LoginForm;

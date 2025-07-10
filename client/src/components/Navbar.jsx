ffa7d8ab8e98806048ed65b5f7ca225461f77921
monahu <monahu2022@gmail.com>
2025-07-10 10:18:15 -0400

    show : {user.displayName}

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,28 @@
-import { ChevronDown, Menu as MenuIcon } from "lucide-react"
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react"
-import { Link } from "react-router"
-import { APP_CONFIG } from "../utils/constants/app"
-import { ROUTES } from "../utils/constants/app"
-import { UI_TEXT } from "../utils/constants/ui"
-import { useNavbar } from "../hooks/useNavbar"
-import { SearchForm } from "./SearchForm"
+import { ChevronDown, Menu as MenuIcon } from 'lucide-react'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Link } from 'react-router'
+import { APP_CONFIG } from '../utils/constants/app'
+import { ROUTES } from '../utils/constants/app'
+import { UI_TEXT } from '../utils/constants/ui'
+import { useNavbar } from '../hooks/useNavbar'
+import { SearchForm } from './SearchForm'
 
 const Navbar = ({ setSidebarOpen }) => {
   // manage navigation bar behavior state
   const { user, userNavigation, hideLogoRoutes, location } = useNavbar()
 
   return (
-    <div className="sticky top-0 z-40 flex h-16 items-center gap-x-4 bg-white px-4 shadow sm:px-6 lg:px-8 border-b  border-gray-200">
+    <div className='sticky top-0 z-40 flex h-16 items-center gap-x-4 bg-white px-4 shadow sm:px-6 lg:px-8 border-b  border-gray-200'>
       {hideLogoRoutes.includes(location.pathname) && (
-        <div className="flex-1">
+        <div className='flex-1'>
           <Link
             to={ROUTES.HOME}
-            className="flex items-center hover:opacity-80 transition-opacity"
+            className='flex items-center hover:opacity-80 transition-opacity'
           >
             <img
               alt={APP_CONFIG.name}
               src={APP_CONFIG.logo}
-              className="h-12 w-auto"
+              className='h-12 w-auto'
             />
           </Link>
         </div>
@@ -32,35 +32,35 @@ const Navbar = ({ setSidebarOpen }) => {
         <>
           <button
             onClick={() => setSidebarOpen(true)}
-            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-            aria-label="Open sidebar menu"
+            className='-m-2.5 p-2.5 text-gray-700 lg:hidden'
+            aria-label='Open sidebar menu'
           >
-            <MenuIcon className="size-6" />
+            <MenuIcon className='size-6' />
           </button>
-          <div className="flex-1 md:hidden mx-auto">
+          <div className='flex-1 md:hidden mx-auto'>
             <Link
               to={ROUTES.HOME}
-              className="flex items-center hover:opacity-80 transition-opacity"
+              className='flex items-center hover:opacity-80 transition-opacity'
             >
               <img
                 alt={APP_CONFIG.name}
                 src={APP_CONFIG.logo}
-                className="h-9 w-auto"
+                className='h-9 w-auto'
               />
             </Link>
           </div>
           {/* Search */}
-          <SearchForm className="hidden md:grid" />
+          <SearchForm className='hidden md:grid' />
         </>
       )}
 
       {/* Right controls */}
-      <div className="flex items-center gap-x-4 ms-auto">
+      <div className='flex items-center gap-x-4 ms-auto'>
         {!user && (
           <>
-            <button className="text-gray-400 hover:text-gray-500">
+            <button className='text-gray-400 hover:text-gray-500'>
               <a
-                className="btn"
+                className='btn'
                 href={ROUTES.LOGIN}
               >
                 {UI_TEXT.login.loginSignup}
@@ -71,32 +71,32 @@ const Navbar = ({ setSidebarOpen }) => {
 
         {user && (
           <Menu>
-            <MenuButton className="flex items-center">
+            <MenuButton className='flex items-center'>
               <img
                 src={user?.photoURL}
                 alt="user's icon"
-                className="size-8 rounded-full"
+                className='size-8 rounded-full'
               />
-              <span className="ml-2 text-sm font-semibold text-gray-900 hidden lg:inline">
-                userName
+              <span className='ml-2 text-sm font-semibold text-gray-900 hidden lg:inline'>
+                {user.displayName}
               </span>
-              <ChevronDown className="ml-1 size-4 text-gray-400 hidden lg:inline" />
+              <ChevronDown className='ml-1 size-4 text-gray-400 hidden lg:inline' />
             </MenuButton>
-            <MenuItems className="absolute right-0 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow ring-1 ring-black/5 focus:outline-none">
+            <MenuItems className='absolute right-0 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow ring-1 ring-black/5 focus:outline-none'>
               {userNavigation.map((item) => (
                 <MenuItem key={item.name}>
                   {item.action ? (
                     <button
-                      type="button"
+                      type='button'
                       onClick={() => item.action()}
-                      className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-50"
+                      className='block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-50'
                     >
                       {item.name}
                     </button>
                   ) : (
                     <Link
                       to={item.href}
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50"
+                      className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50'
                     >
                       {item.name}
                     </Link>
